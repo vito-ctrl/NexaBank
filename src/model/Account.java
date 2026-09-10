c49fd1de1a7e8a49d573528345a1f86f7ffdb19a
vito-ctrl <aymane.elkhadraoui1@gmai.com>
2026-09-10 14:30:10 +0100

    feat:- Add abstract Person class with encapsulated fields and password verification - Add Client and Manager subclasses implementing displayProfile() - Add Account model with validated constructor (negative balance check

--- src/model/Account.java
+++ src/model/Account.java
@@ -1,33 +1,33 @@
-// package model;
+package model;
 
-// import java.util.HashSet;
+import java.util.HashSet;
 
-// public class Account{
-//     private String accountNumber;
-//     private double balance;
-//     private String accountType;
-//     private HashSet<Transaction> transactionHistory;
+public class Account{
+    private String accountNumber;
+    private double balance;
+    private String accountType;
+    private HashSet<Transaction> transactionHistory;
 
-//     public Account(String accountNumber, String accountType, double balance){
-//         this.accountNumber = accountNumber;
-//         this.accountType = accountType;
-//         this.transactionHistory = new HashSet<>();
-//         if(balance < 0)
-//             throw new IllegalArgumentException("the initial balance can't be negative");
-//         else
-//             this.balance = balance;
-//     }
+    public Account(String accountNumber, String accountType, double balance){
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.transactionHistory = new HashSet<>();
+        if(balance < 0)
+            throw new IllegalArgumentException("the initial balance can't be negative");
+        else
+            this.balance = balance;
+    }
 
-//     public String getAccountNumber(){return this.accountNumber;}
-//     public String getAccountType(){return this.accountType;}
-//     public double getBalance(){return this.balance;}
-//     public HashSet<Transaction> getTransactionHistory(){return this.transactionHistory;}
+    public String getAccountNumber(){return this.accountNumber;}
+    public String getAccountType(){return this.accountType;}
+    public double getBalance(){return this.balance;}
+    public HashSet<Transaction> getTransactionHistory(){return this.transactionHistory;}
 
-//     // public void deposit(double amount){
-//     //     try{
-//     //         if(amount < 0)
-//     //             throw new IllegalArgumentException("deposit can't be negative");
-//     //         this.balance += amount;
-//     //     }
-//     // }
-// }
+    // public void deposit(double amount){
+    //     try{
+    //         if(amount < 0)
+    //             throw new IllegalArgumentException("deposit can't be negative");
+    //         this.balance += amount;
+    //     }
+    // }
+}
